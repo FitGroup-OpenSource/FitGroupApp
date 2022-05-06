4310afd4b4f91fc3e7448ee1be8a6f9621c97c13
price <nicole_14_2015@hotmail.com>
2022-05-06 13:43:11 -0500

    Added Types Routines and Added example of exercises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu/menu.component';
 import {HomeComponent} from "./home/home.component";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { RoutinesComponent } from './routines/routines.component';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import { AbsRoutineComponent } from './routines/types-routines/abs-routine/abs-routine.component';
+import {MatListModule} from "@angular/material/list";
+import { ExerciseSupermanComponent } from './routines/exercises/exercise-superman/exercise-superman.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    RoutinesComponent,
+    AbsRoutineComponent,
+    ExerciseSupermanComponent
   ],
   imports: [
     RoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {  HomeComponent } from './home/home.component';
 import {MenuComponent} from "./menu/menu.component";
+import {RoutinesComponent} from "./routines/routines.component";
+import {AbsRoutineComponent} from "./routines/types-routines/abs-routine/abs-routine.component";
+import {ExerciseSupermanComponent} from "./routines/exercises/exercise-superman/exercise-superman.component";
 
 const routes: Routes = [
   { path: 'home',component: HomeComponent},
   { path:'', redirectTo: '/menu', pathMatch:'full'},
   {path: 'menu', component: MenuComponent},
   // { path: '**', component: PageNotFoundComponent }
+  { path: 'routines', component: RoutinesComponent},
+  { path: 'abs-routine', component: AbsRoutineComponent},
+  { path: 'exercise-superman', component: ExerciseSupermanComponent}
 
 ];
 
