d35b0fa52240ad5716cec674f415ee1e8de7a1dd
MAav <vargas_manuel08@hotmail.com>
2022-05-06 18:24:09 -0500

    add.home feature
    home view for new users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu/menu.component';
 import {HomeComponent} from "./home/home.component";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {PhysicalActivityComponent} from "./home/physical-activity/physical-activity.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    PhysicalActivityComponent
   ],
   imports: [
     RoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {  HomeComponent } from './home/home.component';
 import {MenuComponent} from "./menu/menu.component";
+import {NutritionComponent} from "./nutrition/nutrition.component";
+import {PhysicalActivityComponent} from "./home/physical-activity/physical-activity.component";
 
 const routes: Routes = [
   { path: 'home',component: HomeComponent},
   { path:'', redirectTo: '/menu', pathMatch:'full'},
   {path: 'menu', component: MenuComponent},
+  { path:'', redirectTo: 'home', pathMatch:'full'},
+  { path:'', redirectTo: 'home', pathMatch:'full'},
+  {path: 'nutrition', component: NutritionComponent},
+  {path: 'home/activity', component:PhysicalActivityComponent }
   // { path: '**', component: PageNotFoundComponent }
 
 ];
